dbe38b87dfe8e33dec40a9235d9e180774b19051
mehdi <mahdiferroudi@gmail.com>
2023-11-29 15:26:02 +0100

    working version

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
+# Use an official Python runtime as a parent image
 FROM python:3.8
 
+# Set the working directory to /app
 WORKDIR /app
 
-COPY requirements.txt requirements.txt
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install any needed packages specified in requirements.txt
 RUN pip install -r requirements.txt
 
-COPY . .
+# Define environment variable
+ENV NAME sentiment_analysis_container
 
+# Run main.py when the container launches
 CMD ["python", "main.py"]
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,6 +1,11 @@
+# sentiment_analysis.py
 import sys
+import nltk
 from data_loader import load_data
 from sentiment_model import predict_sentiment
+
+# Spécifiez le chemin de téléchargement pour les ressources NLTK
+nltk.data.path.append("/app/nltk_data")
 
 def analyze_sentiment(file_path):
     data = load_data(file_path)
@@ -8,6 +13,9 @@ def analyze_sentiment(file_path):
     return prediction
 
 if __name__ == "__main__":
+    # Assurez-vous que nltk.download fonctionne avec le bon chemin
+    nltk.download("vader_lexicon", download_dir="/app/nltk_data")
+
     if len(sys.argv) != 2:
         print("Usage: python sentiment_analysis.py <path_to_file>")
         sys.exit(1)
